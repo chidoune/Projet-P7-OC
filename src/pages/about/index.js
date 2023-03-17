5c6289cedcb20e40ca409604fda8b5b0f837abf4
Rachida Hajjari <chidoune2011@gmail.com>
2023-03-17 20:52:40 +0100

    projet modifié suite passage jury

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,13 +1,20 @@
 import paysage2 from '../../assets/paysage2.png'
 import Banner from '../../components/Banner/index'
 import Footer from '../../components/Footer/index'
-import DropAllAbout from '../../components/DropAllAbout/index'
+import DropAbout from '../../components/DropAbout/index'
+import {dropdown} from '../../datas/dropdown'
+import '../../styles/DropAbout.css'
 
 function About() {
     return (
       <div>
         <Banner paysage={paysage2} texte = ""/>
-        <DropAllAbout />
+        <div className="dropDownAll">
+          <DropAbout title = "Fiabilité" content ={dropdown[0].content} />
+          <DropAbout title = "Respect" content ={dropdown[1].content} />
+          <DropAbout title = "Service" content ={dropdown[2].content} />
+          <DropAbout title = "Responsabilité" content ={dropdown[3].content} />
+        </div>
         <Footer />
       </div>
     );
